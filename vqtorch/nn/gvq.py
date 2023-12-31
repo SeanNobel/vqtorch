812a59f0e3d1dd320a1ac7e2c49c2f8d1d096a2a
SeanNobel <sensho19950503@gmail.com>
2023-12-31 15:59:12 +0900

    calculate perplexity & reduce loss with mean

--- vqtorch/nn/gvq.py
+++ vqtorch/nn/gvq.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from stringcolor import cs
 from vqtorch.norms import with_codebook_normalization
@@ -7,90 +8,89 @@ from .vq import VectorQuant
 
 
 class GroupVectorQuant(VectorQuant):
-	"""
-	Vector quantization layer.
+    """
+    Vector quantization layer.
 
-	Args:
-		groups (int): Number of groups for vector quantization. The vectors are divided
-			into group chunks. When groups=1, it is the same as VectorQuant.
-		share (bool): If True, codebook is shared for each sub-vector.
-		*rest*: see VectorQuant()
-	"""
+    Args:
+            groups (int): Number of groups for vector quantization. The vectors are divided
+                    into group chunks. When groups=1, it is the same as VectorQuant.
+            share (bool): If True, codebook is shared for each sub-vector.
+            *rest*: see VectorQuant()
+    """
 
-	def __init__(
-			self,
-			feature_size : int,
-			num_codes : int,
-			groups : int = 1,
-			share : bool = True,
-			**kwargs,
-			):
+    def __init__(
+        self,
+        feature_size: int,
+        num_codes: int,
+        groups: int = 1,
+        share: bool = True,
+        **kwargs,
+    ):
+        if not share and not feature_size % groups == 0:
+            e_msg = f"feature_size {self.feature_size} must be divisible by groups {groups}."
+            raise RuntimeError(str(cs(e_msg, "red")))
 
-		if not share and not feature_size % groups == 0:
-			e_msg = f'feature_size {self.feature_size} must be divisible by groups {groups}.'
-			raise RuntimeError(str(cs(e_msg, 'red')))
+        num_codebooks = 1 if share else groups
+        in_dim = self.group_size = num_codes // num_codebooks
+        out_dim = feature_size // groups
 
-		num_codebooks = 1 if share else groups
-		in_dim  = self.group_size = num_codes // num_codebooks
-		out_dim = feature_size // groups
+        super().__init__(feature_size, num_codes, code_vector_size=out_dim, **kwargs)
 
-		super().__init__(feature_size, num_codes, code_vector_size=out_dim, **kwargs)
+        self.groups = groups
+        self.share = share
+        self.codebook = nn.Embedding(in_dim * num_codebooks, out_dim)
+        return
 
-		self.groups = groups
-		self.share = share
-		self.codebook = nn.Embedding(in_dim * num_codebooks, out_dim)
-		return
-	
+    def get_codebook_by_group(self, group):
+        cb = self.codebook.weight
+        offset = 0 if self.share else group * self.group_size
+        return cb[offset : offset + self.group_size], offset
 
-	def get_codebook_by_group(self, group):
-		cb = self.codebook.weight
-		offset = 0 if self.share else group * self.group_size
-		return cb[offset : offset + self.group_size], offset
+    @with_codebook_normalization
+    def forward(self, z):
+        ######
+        ## (1) formatting data by groups and invariant to dim
+        ######
 
+        z = self.prepare_inputs(z, self.groups)
 
-	@with_codebook_normalization
-	def forward(self, z):
+        if not self.enabled:
+            z = self.to_original_format(z)
+            return z, {}
 
-		######
-		## (1) formatting data by groups and invariant to dim
-		######
+        ######
+        ## (2) quantize latent vector
+        ######
 
-		z = self.prepare_inputs(z, self.groups)
+        z_q = torch.zeros_like(z)
+        d = torch.zeros(z_q.shape[:-1]).to(z_q.device)
+        q = torch.zeros(z_q.shape[:-1], dtype=torch.long).to(z_q.device)
 
-		if not self.enabled:
-			z = self.to_original_format(z)
-			return z, {}
+        for i in range(self.groups):
+            # select group
+            _z = z[..., i : i + 1, :]
 
-		######
-		## (2) quantize latent vector
-		######
+            # quantize
+            cb, offset = self.get_codebook_by_group(i)
+            _z_q, _d, _q = self.quantize(cb, _z)
 
-		z_q = torch.zeros_like(z)
-		d = torch.zeros(z_q.shape[:-1]).to(z_q.device)
-		q = torch.zeros(z_q.shape[:-1], dtype=torch.long).to(z_q.device)
+            # assign to tensor
+            z_q[..., i : i + 1, :] = _z_q
+            d[..., i : i + 1] = _d
+            q[..., i : i + 1] = _q + offset
 
-		for i in range(self.groups):
-			# select group
-			_z = z[..., i:i+1, :]
+        e_mean = F.one_hot(q, num_classes=self.num_codes).view(-1, self.num_codes).float().mean(0)  # fmt: skip
+        perplexity = torch.exp(-torch.sum(e_mean * torch.log(e_mean + 1e-10)))
 
-			# quantize
-			cb, offset = self.get_codebook_by_group(i)
-			_z_q, _d, _q = self.quantize(cb, _z)
+        to_return = {
+            "z": z,  # each group input z_e
+            "z_q": z_q,  # quantized output z_q
+            "d": d,  # distance function for each group
+            "q": q,  # codes using offsetted indices
+            "loss": self.compute_loss(z, z_q).mean(),
+            "perplexity": perplexity,  # None,
+        }
 
-			# assign to tensor
-			z_q[..., i:i+1, :] = _z_q
-			d[..., i:i+1] = _d
-			q[..., i:i+1] = _q + offset
-
-		to_return = {
-			'z'   : z,               # each group input z_e
-			'z_q' : z_q,             # quantized output z_q
-			'd'   : d,               # distance function for each group
-			'q'	  : q,				 # codes using offsetted indices
-			'loss': self.compute_loss(z, z_q),
-			'perplexity': None,
-			}
-
-		z_q = self.straight_through_approximation(z, z_q)
-		z_q = self.to_original_format(z_q)
-		return z_q, to_return
+        z_q = self.straight_through_approximation(z, z_q)
+        z_q = self.to_original_format(z_q)
+        return z_q, to_return
